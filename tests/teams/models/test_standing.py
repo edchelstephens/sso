671b85dceb6595813c3fc6517739c1056e44a45e
Edchel Stephen <edchelstephens@gmail.com>
2022-03-23 18:00:45 +0800

    test standing model.

---
+++ tests/teams/models/test_standing.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+from teams.models.standing import Standing
+
+
+class StandingModelTest(TestCase):
+    """Standing model tests case."""
+
+    def setUp(self) -> None:
+        pass
